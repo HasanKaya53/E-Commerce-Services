6f7b5d4175e3da70574a9c789c16a59fe694aacb
Hasan Kaya <hasankayaaa53@gmail.com>
2023-10-02 23:11:44 +0300

    add - delete product

--- Server/controller/productController.js
+++ Server/controller/productController.js
@@ -6,9 +6,15 @@ let product = require('../model/product');
 
 router.post('/newProduct', (req,res)=>{
 
-    let newProduct = new product(req.body.ProductName, req.body.ProductDesc, req.body.ProductPrice, req.body.ProductDiscount, req.body.ProductImgList);
-    newProduct.addProduct();
-    res.status(200).send("Product Added");
+    let newProduct = new product(req.body.ProductName, req.body.ProductDesc, req.body.ProductPrice, req.body.ProductDiscount, req.body.ProductImgList,req.body.ProductStockQuantity);
+    let ProductStatus = newProduct.addProduct();
+
+    if(!ProductStatus){
+        res.status(400).send("Product Already Exists");
+    }else{
+        res.status(200).send("Product Added");
+    }
+
 })
 
 
@@ -18,7 +24,7 @@ router.post('/getAll',(req,res)=>{
 
 
 router.post('/deleteProduct',(req,res)=>{
-    let name = req.res.ProductName;
+    let name = req.body.ProductName;
     if(product.deleteFromName(name)){
         res.status(200).send("Product Deleted");
     }
--- Server/model/product.js
+++ Server/model/product.js
@@ -2,22 +2,33 @@
 const products = [];
 class Product
 {
-    constructor(ProductName, ProductDesc, ProductPrice, ProductDiscount, ProductImgList){
+    constructor(ProductName, ProductDesc, ProductPrice, ProductDiscount, ProductImgList,ProductStockQuantity){
         this.ProductName = ProductName;
         this.ProductDesc = ProductDesc;
         this.ProductPrice = ProductPrice;
         this.ProductDiscount = ProductDiscount;
+        this.ProductStockQuantity = ProductStockQuantity;
         this.ProductImgList = ProductImgList;
     }
 
     addProduct(){
+
+        //if exits product with same name, return false
+        for(let i = 0; i < products.length; i++){
+            if(products[i].ProductName.toLowerCase() == this.ProductName.toLowerCase()){
+                return false;
+            }
+        }
+
         products.push(this);
+        return true;
     }
 
 
     static deleteFromName(name){
         for(let i = 0; i < products.length; i++){
-            if(products[i].ProductName == name){
+            // if exits product with same name, delete it and return true
+            if(products[i].ProductName.toLowerCase() == name.toLowerCase()){
                 products.splice(i, 1);
                 return true;
             }
